36d2307d8cfc114dde7083e1c39f2d63f53c7d80
Nishith Munasinghe <nishithmun@gmail.com>
2024-01-24 19:24:53 +1100

    added a beam break sensor lock to the intake subsystem

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -9,4 +9,5 @@ public class IntakeConstants {
   // Just a random number
   public static final MotorBuilder<WPI_TalonSRX> MOTOR_1_ID = new TalonMotorBuilder(7912);
   public static final MotorBuilder<WPI_TalonSRX> MOTOR_2_ID = new TalonMotorBuilder(7913);
+  public static final int BEAM_BREAK_SENSOR_ID = 78;
 }
--- src/main/java/frc/robot/subsystems/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/IntakeSub.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.constants.IntakeConstants;
@@ -9,6 +10,8 @@ import frc.robot.constants.IntakeConstants;
 public class IntakeSub extends SubsystemBase {
   private final WPI_TalonSRX masterMotor = IntakeConstants.MOTOR_1_ID.build();
   private final WPI_TalonSRX slaveMotor = IntakeConstants.MOTOR_2_ID.build();
+  private final DigitalInput beamBreakSensor = new DigitalInput(IntakeConstants.BEAM_BREAK_SENSOR_ID);
+  private boolean locked = false;
 
   public IntakeSub() {
     masterMotor.configFactoryDefault();
@@ -17,11 +20,19 @@ public class IntakeSub extends SubsystemBase {
     slaveMotor.follow(masterMotor);
   }
 
+  @Override
+  public void periodic() {
+    boolean robotContainsNote = beamBreakSensor.get();
+    locked = robotContainsNote;
+  }
+
   public void set(double newSpeed) {
+    if (locked) return;
     masterMotor.set(newSpeed);
   }
 
   public void stop() {
+    if (locked) return;
     masterMotor.stopMotor();
   }
 }
