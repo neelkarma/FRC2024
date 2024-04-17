0a0904f7f6e557d51190dc614c9d5e174d32f518
Neel Sharma <nsdj.sharma@gmail.com>
2024-04-17 17:18:30 +1000

    clean up intake led code

--- src/main/java/frc/robot/subsystems/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/IntakeSub.java
@@ -5,9 +5,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Subsystems;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.ClearLEDCommand;
+import frc.robot.commands.SolidLEDCommand;
 import frc.robot.constants.IntakeConstants;
 import frc.robot.shufflecontrol.ShuffleControl;
 
@@ -16,14 +17,16 @@ public class IntakeSub extends SubsystemBase {
   private final WPI_VictorSPX slaveMotor = IntakeConstants.LOWER_MOTOR_ID.get();
   private final DigitalInput beamBreakSensor = new DigitalInput(IntakeConstants.BEAM_BREAK_SENSOR_ID);
 
-  private boolean locked = false;
-  private boolean ledState = !locked;
-
   public IntakeSub() {
     masterMotor.configFactoryDefault();
     slaveMotor.configFactoryDefault();
 
     slaveMotor.follow(masterMotor);
+
+    // notelock leds
+    new Trigger(() -> noteIsPresent())
+        .onTrue(new SolidLEDCommand(new Color("#00ff00")))
+        .onFalse(new ClearLEDCommand());
 
     addChild("Master Motor", masterMotor);
     addChild("Slave Motor", slaveMotor);
@@ -33,24 +36,9 @@ public class IntakeSub extends SubsystemBase {
   @Override
   public void periodic() {
     final var isRunning = masterMotor.get() != 0.0;
+    final var locked = noteIsPresent();
 
     ShuffleControl.miscTab.setIntakeVars(locked, isRunning);
-    locked = noteIsPresent();
-    if (locked && !ledState) {
-      for (int i = 0; i < Subsystems.led.buffer.getLength(); i++) {
-        Subsystems.led.buffer.setLED(i, new Color("#00ff00"));
-      }
-      Subsystems.led.apply();
-      ledState = true;
-    } else if(!locked && ledState) {
-      for (int i = 0; i < Subsystems.led.buffer.getLength(); i++) {
-        Subsystems.led.buffer.setLED(i, new Color("#000000"));
-      }
-      Subsystems.led.apply();
-      ledState = false;
-    }
-    //if (isRunning && locked)
-    //  stop();
   }
 
   /**
